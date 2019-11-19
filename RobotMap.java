20318385a815208ed06a770cfe12d3c5541509d6
Brady Nichols <school.bnichols@gmail.com>
2019-11-19 15:55:18 -0500

    Added some comments

--- RobotMap.java
+++ RobotMap.java
@@ -1,6 +1,18 @@
-//ROBOT MAP
+// THE ROBOT MAP CLASS
 
+/* 
+The RobotMap class is the class that sets the variables for each part of the robot (motors, servos, etc.).
+When we start an autonomous or a teleop mode, we must first create the 'robot' object.
+This robot object is created from the RobotMap class, defined in this file.
+
+*/
+
+
+
+// GOBILDA Password: matesrobotics123
 // Copyright 2018-2019 FIRST Tech Challenge Team 12365
+
+// Importing like everything
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.hardware.DcMotor;
@@ -15,26 +27,28 @@ import com.qualcomm.robotcore.hardware.TouchSensor;
 import com.qualcomm.robotcore.hardware.DistanceSensor;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
+// Initializing the class
 public class RobotMap {
 
-    /* Public OpMode members. */
+    // Instantiating variables of all of the motors
     public DcMotor leftFrontDrive = null;
     public DcMotor rightFrontDrive = null;
     public DcMotor leftBackDrive = null;
     public DcMotor rightBackDrive = null;
     public DcMotor claw = null;
 
-    /* local OpMode members. */
+    // Initializing elapsedtime (for use in drive by time)
     private ElapsedTime period  = new ElapsedTime();
 
-    /* Constructor */
+    // Constructur
     public RobotMap(){
+        // This is empty because nothing needs to be done during robot creation
+        // Also takes no parameters
     }
 
     /* Initialize standard Hardware interfaces */
     public void init(HardwareMap hwMap) {
 
-        // Save reference to Hardware map
 
         // Define and Initialize Motors
         leftFrontDrive = hwMap.get(DcMotor.class, "leftFrontDrive");
@@ -49,7 +63,7 @@ public class RobotMap {
         rightBackDrive.setDirection(DcMotor.Direction.FORWARD);
         claw.setDirection(DcMotor.Direction.FORWARD);
        
-        //Set all motors to zero power
+        //Set all motors to zero power just in case
         leftFrontDrive.setPower(0);
         rightFrontDrive.setPower(0);
         leftBackDrive.setPower(0);
@@ -63,7 +77,7 @@ public class RobotMap {
         rightFrontDrive.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         claw.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         
-        //WHEN USING ENCODER THE TICKS ARE 1440
+        // WHEN USING ENCODER THE TICKS ARE 1440
     }
 }
 
